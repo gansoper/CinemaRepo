a1d880640d9a2c0f8194aa86d301e547cc504d0c
Andrey Guselnikov <gansoper@gmail.com>
2021-04-30 07:21:50 +0300

    Updates for Swagger. Fix the News Controller

--- src/main/java/com/example/cinema/CinemaApplication.java
+++ src/main/java/com/example/cinema/CinemaApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
-@EnableSwagger2
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class CinemaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
--- src/main/java/com/example/cinema/controller/NewsController.java
+++ src/main/java/com/example/cinema/controller/NewsController.java
@@ -2,6 +2,8 @@ package com.example.cinema.controller;
 
 import com.example.cinema.dto.NewsDTO;
 import com.example.cinema.dto.NewsDetailsDTO;
+import com.example.cinema.service.NewsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,18 +15,21 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/v1/news")
+@RequiredArgsConstructor
 public class NewsController {
+
+    private final NewsService newsService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public Page<NewsDTO> getNews(@PageableDefault(size = 10) Pageable pageable, @RequestParam(value = "searchText", required = false) String searchText) {
-        return null;
+        return newsService.getNews(pageable, searchText);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public NewsDetailsDTO getNewsDetails(@PathVariable("id") String id) {
-        return null;
+        return newsService.getNewsDetails(id);
     }
 
 }
